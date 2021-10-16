82db0f240b4756985a262ead5c1ecf12580cfb7b
Tx000 <52128167+Tx000@users.noreply.github.com>
2021-10-16 11:03:16 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,30 +1,12 @@
-# Original Version: Taehoon Kim (http://carpedm20.github.io)
-#   + Source: https://github.com/carpedm20/DCGAN-tensorflow/blob/e30539fb5e20d5a0fed40935853da97e9e55eee8/utils.py
-#   + License: MIT
-# [2017-07] Modifications for sText2Image: Shangzhe Wu
-#   + License: MIT
+"""
+Some codes from https://https://github.com/elliottwu/DeepHDR
+"""
 
-"""
-Some codes from https://github.com/Newmu/dcgan_code
-"""
-from __future__ import division
-import math
-import json
-import random
-import pprint
 import cv2
 import numpy as np
-from time import gmtime, strftime
+import h5py
+import torch
 
-import pdb
-
-
-def compute_psnr(img1, img2):
-    mse = np.mean( (img1 - img2) ** 2 )
-    if mse == 0:
-        return 100
-    PIXEL_MAX = 2.0 # input -1~1
-    return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
 
 def radiance_writer(out_path, image):
     with open(out_path, "wb") as f:
@@ -43,13 +25,54 @@ def radiance_writer(out_path, image):
         rgbe.flatten().tofile(f)
 
 
-pp = pprint.PrettyPrinter()
 
-get_stddev = lambda x, k_h, k_w: 1/math.sqrt(k_w*k_h*x.get_shape()[-1])
-GAMMA = 2.2
+MU = 5000. # tunemapping parameter
+def tonemap_np(images):  # input/output 0~1
+    return np.log(1 + MU * images) / np.log(1 + MU)
 
+
+def tonemap(images):  # input/output 0~1
+    return torch.log(1.0 + MU * images) / np.log(1.0 + MU)
+
+
+GAMMA = 2.2 # LDR&HDR domain transform parameter
 def LDR2HDR(img, expo): # input/output 0~1
-    return (img**GAMMA / expo)
+    return img ** GAMMA / expo
+
+
+def arrenge(img):
+    H, W, C = img.shape
+    img_ = np.zeros(shape=(W, H, C))
+    for i in range(W):
+        for j in range(H):
+            img_[i, j, :] = img[j, i, :]
+    return img_
+
+
+def center_crop(x, image_size):
+    crop_h, crop_w = image_size
+    _, _, h, w = x.shape
+    j = int(round((h - crop_h)/2.))
+    i = int(round((w - crop_w)/2.))
+    return x[:, :, max(0,j):min(h,j+crop_h), max(0,i):min(w,i+crop_w)]
+
+
+def resize(x, scale_factor):
+    _, _, h, w = x.shape
+    x = np.transpose(np.squeeze(x), axes=(1, 2, 0))
+    x = np.clip(cv2.resize(x, dsize=(w // scale_factor, h // scale_factor), interpolation=cv2.INTER_CUBIC), 0, 1)
+    x = np.expand_dims(np.transpose(x, axes=(2, 0, 1)), axis=0)
+    return x
+
+
+def get_input(scene_dir, image_size, scale_factor):
+    hf = h5py.File(scene_dir)
+    inputs = np.array(hf.get('IN'))
+    inputs = center_crop(inputs, image_size)
+    inputs = resize(inputs, scale_factor)
+    ref_HDR = np.array(hf.get('GT'))
+    ref_HDR = center_crop(ref_HDR, image_size)
+    return inputs, ref_HDR
 
 def get_image2HDR(image_path, exp, image_size=None, is_crop=False):
     if is_crop:
@@ -91,12 +114,6 @@ def imsave(images, path):
     else:
         return cv2.imwrite(path, images[...,::-1]*255.)
 
-def center_crop(x, image_size):
-    crop_h, crop_w = image_size
-    h, w = x.shape[:2]
-    j = int(round((h - crop_h)/2.))
-    i = int(round((w - crop_w)/2.))
-    return cv2.resize(x[max(0,j):min(h,j+crop_h), max(0,i):min(w,i+crop_w)], (crop_w, crop_h))
 
 def transform(image, image_size, is_crop, is_SR=False):
     # npx : # of pixels width/height of image
@@ -112,139 +129,3 @@ def transform(image, image_size, is_crop, is_SR=False):
 
 def inverse_transform(images):
     return (images+1.)/2.
-
-def to_json(output_path, *layers):
-    with open(output_path, "w") as layer_f:
-        lines = ""
-        for w, b, bn in layers:
-            layer_idx = w.name.split('/')[0].split('h')[1]
-
-            B = b.eval()
-
-            if "lin/" in w.name:
-                W = w.eval()
-                depth = W.shape[1]
-            else:
-                W = np.rollaxis(w.eval(), 2, 0)
-                depth = W.shape[0]
-
-            biases = {"sy": 1, "sx": 1, "depth": depth, "w": ['%.2f' % elem for elem in list(B)]}
-            if bn != None:
-                gamma = bn.gamma.eval()
-                beta = bn.beta.eval()
-
-                gamma = {"sy": 1, "sx": 1, "depth": depth, "w": ['%.2f' % elem for elem in list(gamma)]}
-                beta = {"sy": 1, "sx": 1, "depth": depth, "w": ['%.2f' % elem for elem in list(beta)]}
-            else:
-                gamma = {"sy": 1, "sx": 1, "depth": 0, "w": []}
-                beta = {"sy": 1, "sx": 1, "depth": 0, "w": []}
-
-            if "lin/" in w.name:
-                fs = []
-                for w in W.T:
-                    fs.append({"sy": 1, "sx": 1, "depth": W.shape[0], "w": ['%.2f' % elem for elem in list(w)]})
-
-                lines += """
-                    var layer_%s = {
-                        "layer_type": "fc",
-                        "sy": 1, "sx": 1,
-                        "out_sx": 1, "out_sy": 1,
-                        "stride": 1, "pad": 0,
-                        "out_depth": %s, "in_depth": %s,
-                        "biases": %s,
-                        "gamma": %s,
-                        "beta": %s,
-                        "filters": %s
-                    };""" % (layer_idx.split('_')[0], W.shape[1], W.shape[0], biases, gamma, beta, fs)
-            else:
-                fs = []
-                for w_ in W:
-                    fs.append({"sy": 5, "sx": 5, "depth": W.shape[3], "w": ['%.2f' % elem for elem in list(w_.flatten())]})
-
-                lines += """
-                    var layer_%s = {
-                        "layer_type": "deconv",
-                        "sy": 5, "sx": 5,
-                        "out_sx": %s, "out_sy": %s,
-                        "stride": 2, "pad": 1,
-                        "out_depth": %s, "in_depth": %s,
-                        "biases": %s,
-                        "gamma": %s,
-                        "beta": %s,
-                        "filters": %s
-                    };""" % (layer_idx, 2**(int(layer_idx)+2), 2**(int(layer_idx)+2),
-                             W.shape[0], W.shape[3], biases, gamma, beta, fs)
-        layer_f.write(" ".join(lines.replace("'","").split()))
-
-def make_gif(images, fname, duration=2, true_image=False):
-  import moviepy.editor as mpy
-
-  def make_frame(t):
-    try:
-      x = images[int(len(images)/duration*t)]
-    except:
-      x = images[-1]
-
-    if true_image:
-      return x.astype(np.uint8)
-    else:
-      return ((x+1)/2*255).astype(np.uint8)
-
-  clip = mpy.VideoClip(make_frame, duration=duration)
-  clip.write_gif(fname, fps = len(images) / duration)
-
-def visualize(sess, dcgan, config, option):
-  if option == 0:
-    z_sample = np.random.uniform(-0.5, 0.5, size=(config.batch_size, dcgan.z_dim))
-    samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
-    save_images(samples, [8, 8], './samples/test_%s.png' % strftime("%Y-%m-%d %H:%M:%S", gmtime()))
-  elif option == 1:
-    values = np.arange(0, 1, 1./config.batch_size)
-    for idx in xrange(100):
-      print(" [*] %d" % idx)
-      z_sample = np.zeros([config.batch_size, dcgan.z_dim])
-      for kdx, z in enumerate(z_sample):
-        z[idx] = values[kdx]
-
-      samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
-      save_images(samples, [8, 8], './samples/test_arange_%s.png' % (idx))
-  elif option == 2:
-    values = np.arange(0, 1, 1./config.batch_size)
-    for idx in [random.randint(0, 99) for _ in xrange(100)]:
-      print(" [*] %d" % idx)
-      z = np.random.uniform(-0.2, 0.2, size=(dcgan.z_dim))
-      z_sample = np.tile(z, (config.batch_size, 1))
-      #z_sample = np.zeros([config.batch_size, dcgan.z_dim])
-      for kdx, z in enumerate(z_sample):
-        z[idx] = values[kdx]
-
-      samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
-      make_gif(samples, './samples/test_gif_%s.gif' % (idx))
-  elif option == 3:
-    values = np.arange(0, 1, 1./config.batch_size)
-    for idx in xrange(100):
-      print(" [*] %d" % idx)
-      z_sample = np.zeros([config.batch_size, dcgan.z_dim])
-      for kdx, z in enumerate(z_sample):
-        z[idx] = values[kdx]
-
-      samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
-      make_gif(samples, './samples/test_gif_%s.gif' % (idx))
-  elif option == 4:
-    image_set = []
-    values = np.arange(0, 1, 1./config.batch_size)
-
-    for idx in xrange(100):
-      print(" [*] %d" % idx)
-      z_sample = np.zeros([config.batch_size, dcgan.z_dim])
-      for kdx, z in enumerate(z_sample): z[idx] = values[kdx]
-
-      image_set.append(sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample}))
-      make_gif(image_set[-1], './samples/test_gif_%s.gif' % (idx))
-
-    new_image_set = [merge(np.array([images[idx] for images in image_set]), [10, 10]) \
-        for idx in range(64) + range(63, -1, -1)]
-    make_gif(new_image_set, './samples/test_gif_merged.gif', duration=8)
-
-def rgb2gray(rgb):
-    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
